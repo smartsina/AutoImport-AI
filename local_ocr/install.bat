4ba48540902b88aa10c03eddb9865723c2803446
smartsina <smartsina92@gmail.com>
2026-07-22 13:14:21 +0330

    add windows scripts

---
+++ local_ocr/install.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Installing OCR Server requirements for Windows...
+echo.
+
+pip install -r requirements.txt
+if errorlevel 1 (
+    echo.
+    echo Failed to install dependencies. Make sure Python and PIP are installed and added to PATH.
+    pause
+    exit /b 1
+)
+
+echo.
+echo Installation successful! You can now run start.bat
+pause
